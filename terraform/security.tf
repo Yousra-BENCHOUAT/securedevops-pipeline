dec6e2f201f3262cb3532cd1930f8afa5f1b81ba
yousra <benchouatyousra@gmail.com>
2026-04-29 01:25:48 +0100

    complete: SecureDevOps Pipeline final

--- terraform/security.tf
+++ terraform/security.tf
@@ -42,6 +42,14 @@ resource "aws_security_group" "main" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "App"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
